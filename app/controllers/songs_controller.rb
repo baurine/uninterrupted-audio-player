e8fc399c52b00e148e2f48af09887df4ca8ac1ed
baurine <2008.hbl@gmail.com>
2018-10-01 14:32:10 +0800

    Add routes

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,11 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+    render json: @songs
+  end
+
+  def show
+    @song = Song.find params[:id]
+    render json: @song
+  end
+end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,3 @@
 class Song < ApplicationRecord
+  validates :title, :cover_url, :audio_url, presence: true
 end
